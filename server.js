8249c7044bccc17bc85b377afaf63624103727ed
Dion Dirza <dion.dirza@veritrans.co.id>
2016-07-05 11:46:08 +0700

    add cache control for 1 year

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(compression());
+app.use(function (req, res, next) {
+    res.header('Cache-Control', 'public, max-age=31536000');
+    next();
+});
+
 app.use(express.static(root));
 app.use(fallback('index.html', { root }));
-
 app.listen(app.get('port'), () => {
     console.log(`Node app is running on port ${app.get('port')}`);
 });
